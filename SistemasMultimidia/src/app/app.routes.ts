c4cebfd4b40b0bdc8c6379e59c917360d7e49a52
gui-carrazzoni <carrazzoni.guilherme@gmail.com>
2024-12-03 14:52:17 -0300

    Criação das duas paginas
    Criando a pagina para atividades e para a home

--- SistemasMultimidia/src/app/app.routes.ts
+++ SistemasMultimidia/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { AtividadesComponent } from './atividades/atividades.component';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
 export const routes: Routes = [
-  { path: '', component: AppComponent },
+  {path: 'home', component: HomeComponent},
   { path: 'atividades', component: AtividadesComponent },
 ];
