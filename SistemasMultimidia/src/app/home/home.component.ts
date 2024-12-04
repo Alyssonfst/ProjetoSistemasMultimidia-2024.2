3a774278311e00247b9ed87c5ecb86b4dbfc3d2f
gui-carrazzoni <carrazzoni.guilherme@gmail.com>
2024-12-04 00:29:00 -0300

    melhoria grafica

--- SistemasMultimidia/src/app/home/home.component.ts
+++ SistemasMultimidia/src/app/home/home.component.ts
@@ -13,16 +13,20 @@ import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js';
 export class HomeComponent implements OnInit {
   title = 'SistemasMultimidia';
 
-  weight: number = 70; 
-  height: number = 1.75; 
-  gender: 'male' | 'female' = 'male';
+  weight: number = 100; 
+  height: number = 1.75;
+  gordura_percent: number = 38.3;
+  gordura_visceral: number = 68.46;
+  cintura_estatura: number = Number((this.weight/this.height).toFixed(2))
+  gender: 'male' | 'female' = 'female';
+  imc: number = Number(this.calculateBMI().toFixed(2))
+
 
   private scene!: THREE.Scene;
   private camera!: THREE.PerspectiveCamera;
   private renderer!: THREE.WebGLRenderer;
   private model!: THREE.Object3D;
 
-  private chart: any;
 
   ngOnInit() {
     this.initialize3DModel();
@@ -75,10 +79,10 @@ export class HomeComponent implements OnInit {
       else if (bmi < 30) modelPath = 'assets/Heavy_Man_Lores.fbx';
       else modelPath = 'assets/Obese_Man_Lores.fbx';
     } else {
-      if (bmi < 18.5) modelPath = 'assets/Skinny_Female_Lores.fbx';
-      else if (bmi < 25) modelPath = 'assets/Slender_Female_Lores.fbx';
-      else if (bmi < 30) modelPath = 'assets/Heavy_Female_Lores.fbx';
-      else modelPath = 'assets/Obese_Female_Lores.fbx';
+      if (bmi < 18.5) modelPath = 'assets/Skinny_Woman_Lores.fbx';
+      else if (bmi < 25) modelPath = 'assets/Slender_Woman_Lores.fbx';
+      else if (bmi < 30) modelPath = 'assets/Heavy_Woman_Lores.fbx';
+      else modelPath = 'assets/Obese_Woman_Lores.fbx';
     }
   
     loader.load(
@@ -119,15 +123,16 @@ export class HomeComponent implements OnInit {
 
   createCharts() {
     const visceralCtx = document.getElementById('grafico-visceral') as HTMLCanvasElement;
-    const cinturaEstatura = document.getElementById('grafico-cintura-estatura') as HTMLCanvasElement;
-    const gorduraPercent = document.getElementById('grafico-gordura-percent') as HTMLCanvasElement;
+    const cinturaEstaturaCtx = document.getElementById('grafico-cintura-estatura') as HTMLCanvasElement;
+    const gorduraPercentCtx = document.getElementById('grafico-gordura-percent') as HTMLCanvasElement;
+
     new Chart(visceralCtx, {
       type: 'doughnut',
       data: {
         labels: ['Gordura Visceral', 'Resto do Corpo'],
         datasets: [
           {
-            data: [68.46, 100 - 68.46],
+            data: [this.gordura_visceral, 100 - this.gordura_visceral],
             backgroundColor: ['#ffcc00', '#444'],
             borderWidth: 0,
             hoverOffset: 4,
@@ -150,14 +155,15 @@ export class HomeComponent implements OnInit {
         },
       },
     });
-  
-    new Chart(gorduraPercent, {
+
+    const cinturaEstaturaRatio = Number((this.weight / this.height).toFixed(2));
+    new Chart(cinturaEstaturaCtx, {
       type: 'doughnut',
       data: {
-
+        labels: ['Cintura/Estatura', 'Resto'],
         datasets: [
           {
-            data: [50, 100 - 50],
+            data: [cinturaEstaturaRatio, 100 - cinturaEstaturaRatio],
             backgroundColor: ['#ffcc00', '#444'],
             borderWidth: 0,
             hoverOffset: 4,
@@ -165,9 +171,10 @@ export class HomeComponent implements OnInit {
         ],
       },
       options: {
-        responsive: true, 
-        circumference: 180, 
-        cutout: '80%',  
+        responsive: true,
+        rotation: -90,
+        circumference: 180,
+        cutout: '80%',
         plugins: {
           legend: { 
             display: true, 
@@ -178,15 +185,15 @@ export class HomeComponent implements OnInit {
           },
         },
       },
-    });    
-    
-    new Chart(cinturaEstatura, {
+    });
+
+    new Chart(gorduraPercentCtx, {
       type: 'doughnut',
       data: {
-
+        labels: ['Gordura Corporal', 'Massa Magra'],
         datasets: [
           {
-            data: [50, 100 - 50],
+            data: [this.gordura_percent, 100 - this.gordura_percent],
             backgroundColor: ['#ffcc00', '#444'],
             borderWidth: 0,
             hoverOffset: 4,
@@ -195,9 +202,8 @@ export class HomeComponent implements OnInit {
       },
       options: {
         responsive: true,
-        rotation: -90, 
-        circumference: 180, 
-        cutout: '80%',  
+        circumference: 180,
+        cutout: '80%',
         plugins: {
           legend: { 
             display: true, 
@@ -211,18 +217,27 @@ export class HomeComponent implements OnInit {
     });
   }
 
-  updateParameters(weight: number, height: number, gender: 'male' | 'female') {
+  updateParameters(weight: number, height: number, gender: 'male' | 'female', gordura_percent: number, gordura_visceral: number) {
     this.weight = weight;
     this.height = height;
     this.gender = gender;
+    this.gordura_percent = gordura_percent;
+    this.gordura_visceral = gordura_visceral;
 
     this.loadModelBasedOnBMI();
 
-    if (this.chart) {
-      const bmi = this.calculateBMI();
-      this.chart.data.datasets[0].data = [this.weight, this.height, bmi];
-      this.chart.update();
-    }
+    const visceralChart = Chart.getChart('grafico-visceral')!;
+    visceralChart.data.datasets[0].data = [this.gordura_visceral, 100 - this.gordura_visceral];
+    visceralChart.update();
+
+    const cinturaEstaturaChart = Chart.getChart('grafico-cintura-estatura')!;
+    const cinturaEstaturaRatio = this.weight / this.height;
+    cinturaEstaturaChart.data.datasets[0].data = [cinturaEstaturaRatio, 100 - cinturaEstaturaRatio];
+    cinturaEstaturaChart.update();
+
+    const gorduraPercentChart = Chart.getChart('grafico-gordura-percent')!;
+    gorduraPercentChart.data.datasets[0].data = [this.gordura_percent, 100 - this.gordura_percent];
+    gorduraPercentChart.update();
   }
 
   animate() {
